145fd0954f3208562320c94c1471397e9ddbce47
natabe1la <147366746+natabe1la@users.noreply.github.com>
2026-01-26 23:40:54 +0800

    login.php

--- login.php
+++ login.php
@@ -3,23 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
-    <div class="profile">
+    <div class="login">
     <?php
-        echo "<h1>Login<br><br></h1>";
-        /*echo "<p>Please enter your username and password to login.</p>";*/
+        echo "<h1>Login to your account</h1>";
+        echo "<p>Please enter your username and password to login.</p>";
     ?>
 
     <form method="post" action="display.php">
+        <div class="space">
         <label> Username:</label>
-        <input type="text" name="username" placeholder="Username" required><br>
-        <label> Password:</label>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <input type="submit" value="Login">
+        <input type="text" name="username" placeholder="Username" class="l_label" required>
+        </div>
+
+        <div class="space">
+        <label class> Password:</label>
+        <input type="password" name="password" placeholder="Password" class="l_label" required>
+        </div>
+       <button type="submit" class="button">
+            Login
+        </button>
     </form>
+    <p> Dont have an account? <a href="index.php">Register here</a></p>
     </div>
 </body>
 </html>
