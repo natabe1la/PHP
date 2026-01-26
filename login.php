19d964dd29cfa48b31c1915a62e02a381bdf2b7b
natabe1la <147366746+natabe1la@users.noreply.github.com>
2026-01-26 23:21:06 +0800

    login.php

--- login.php
+++ login.php
@@ -3,21 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
 </head>
 <body>
+    <div class="profile">
     <?php
         echo "<h1>Login<br><br></h1>";
+        /*echo "<p>Please enter your username and password to login.</p>";*/
     ?>
 
     <form method="post" action="display.php">
-        Please enter your login details: <br>
         <label> Username:</label>
         <input type="text" name="username" placeholder="Username" required><br>
         <label> Password:</label>
         <input type="password" name="password" placeholder="Password" required><br><br>
         <input type="submit" value="Login">
     </form>
+    </div>
 </body>
 </html>
-
